c3e8e2935cfc656b49feb5e232c83989c74d8cd1
Tamara Khliabets <tamara_khliabets@epam.com>
2021-03-22 00:04:28 +0300

    init: start task 1

---
+++ task_1_1.js
@@ -0,0 +1,9 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+      input: process.stdin,
+});
+
+rl.on('line', line => {
+      console.log(line.split('').reverse().join(''));
+})
